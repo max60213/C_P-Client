34210aaf170633994f695b7cb46e9d044f2236ab
max60213 <max60213@gmail.com>
2022-12-05 00:40:53 +0800

    Directly send from clipboard

--- app/src/main/java/com/example/c_pcombine/MainActivity.java
+++ app/src/main/java/com/example/c_pcombine/MainActivity.java
@@ -24,15 +24,12 @@ public class MainActivity extends AppCompatActivity {
     // declaring required variables
     private Socket client;
     private PrintWriter printwriter;
-    private EditText textField;
     private Button button;
     private String message;
     private EditText editIp;
     private String ipAddress;
 
     // Clipboard
-    TextInputLayout tilData;
-    TextView txtResult;
     private final String CLIPBOARD_LABEL = "CLIPBOARD_LABEL";
     private ClipboardManager clipboardManager;
     StringBuilder stringBuilder = new StringBuilder();
@@ -48,15 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
         //--------------------- Clipboard ----------------------
 
-        tilData = findViewById(R.id.tilData);
-        txtResult = findViewById(R.id.txtResult);
 
         clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
 
         //---------------------- Client -----------------------
 
-        // reference to the text field
-        textField = (EditText) findViewById(R.id.ediText1);
 
         // reference to the send button
         button = (Button) findViewById(R.id.button1);
@@ -65,27 +58,13 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                // clipboard
-                String data = tilData.getEditText().getText().toString();
-
-                if(!TextUtils.isEmpty(data)){
-                    ClipData clip = ClipData.newPlainText(CLIPBOARD_LABEL, data);
-                    clipboardManager.setPrimaryClip(clip);
-
-                    for(int i = 0; i < clipboardManager.getPrimaryClip().getItemCount(); i++){
-                        txtResult.setText(clipboardManager.getPrimaryClip().getItemAt(i).getText() + "\n");
-                    }
-
-                    // Show copied text
-//                    txtResult.setText(clipboardManager.getPrimaryClip().getItemAt(i).getText() + "\n");
-
-                } else {
-                    Toast.makeText(MainActivity.this, "Filed is empty", Toast.LENGTH_LONG).show();
-                }
-
                 // Send message
                 // get the text message on the text field
-                message = textField.getText().toString();
+                //message = textField.getText().toString();
+
+                // get the message from clipboard
+                message = clipboardManager.getPrimaryClip().getItemAt(0).getText().toString();
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
                 ipAddress = editIp.getText().toString();
 
                 // start the Thread to connect to server
@@ -121,14 +100,6 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            // updating the UI
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    textField.setText("");
-                }
-            });
         }
     }
 }
